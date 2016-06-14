0ff6ef17da6f3701d89bb9d3e2de5d2060d002b7
Pete <peter.s.giles@gmail.com>
2016-06-14 18:03:01 +1000

    cards and images

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -25,7 +25,7 @@ gulp.task('build-html', function() {
         .pipe(plugins.uglify())
         .pipe(gulp.dest(paths.outputAssets + '/js'));
 
-    var appCssStream = gulp.src(['./app/css/*.css'])
+    var appCssStream = gulp.src(['./app/css/styles.css'])
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.cleanCss({
             debug: true,
